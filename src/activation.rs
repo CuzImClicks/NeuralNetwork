27bd64152ceb617e25fbbfd0c3a2813462207b92
CuzImClicks <58398364+CuzImClicks@users.noreply.github.com>
2024-02-28 23:32:38 +0100

    fixed printing out of stats

---
+++ src/activation.rs
@@ -0,0 +1,24 @@
+
+pub fn sigmoid(x: f64) -> f64 {
+    1.0 / (1.0 + (-x).exp())
+}
+
+pub fn sigmoid_prime(x: f64) -> f64 {
+    sigmoid(x) * (1.0 - sigmoid(x))
+}
+
+pub fn relu(x: f64) -> f64 {
+    if x > 0.0 {
+        x
+    } else {
+        0.0
+    }
+}
+
+pub fn relu_prime(x: f64) -> f64 {
+    if x > 0.0 {
+        1.0
+    } else {
+        0.0
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![feature(iter_next_chunk)]
 
+mod activation;
+
 use std::fmt::Display;
-use ndarray::{arr2, Array, Array2, ArrayBase, Ix2, OwnedRepr};
+use ndarray::{arr2, Array2, ArrayBase, Ix2, OwnedRepr};
 use rand::prelude::SliceRandom;
 use rand::Rng;
+use crate::activation::{sigmoid, sigmoid_prime};
 
 struct NeuralNetwork {
     weights: Vec<Array2<f64>>,
@@ -18,17 +21,7 @@ fn gen_random_matrix(rows: usize, cols: usize) -> Array2<f64>
     Array2::from_shape_fn((rows, cols), |_| rng.gen_range(0.0..1.0))
 }
 
-fn sigmoid(x: f64) -> f64 {
-    1.0 / (1.0 + (-x).exp())
-}
 
-fn sigmoid_vec(x: &Array2<f64>) -> Array2<f64> {
-    x.mapv(sigmoid)
-}
-
-fn sigmoid_prime(x: f64) -> f64 {
-    sigmoid(x) * (1.0 - sigmoid(x))
-}
 
 fn cost_derivative(output: ArrayBase<OwnedRepr<f64>, Ix2>, truth: &Array2<f64>) -> Array2<f64> {
     output - truth
@@ -48,7 +41,7 @@ impl NeuralNetwork {
     fn feedforward(&self, inputs: Array2<f64>) -> Array2<f64> {
         let mut result = inputs;
         for (layer, bias) in self.weights.iter().zip(self.biases.iter()) {
-            result = sigmoid_vec(&(&layer.dot(&result) + bias));
+            result = (&layer.dot(&result) + bias).mapv(sigmoid);
         }
         result
     }
@@ -129,42 +122,31 @@ fn mirror_zeros<O: num_traits::Zero + Clone>(i: &[Array2<O>]) -> Vec<Array2<O>> 
 
 fn main() {
     let mut n = NeuralNetwork::initialise_random(vec![2, 3, 1]);
-    //let n = NeuralNetwork {
-    //    weights: vec![arr2(&[[0.41, 0.5], [0.94, 0.57], [0.24, 0.5]]), arr2(&[[0.7, 0.8, 0.9]])],
-    //    biases: vec![arr2(&[[0.55], [0.82], [0.16]]), arr2(&[[0.92]])]
-    //};
-    //let n = NeuralNetwork {
-    //    weights: vec![arr2(&[[1.91, 2.82], [2.65, 4.16]]), arr2(&[[0.55, 0.82]])],
-    //    biases: vec![arr2(&[[0.55], [0.82]]), arr2(&[[0.92]])]
-    //};
-    //let mut n = NeuralNetwork {
-    //    weights: vec![arr2(&[[-6.48660725, -6.62301231],  [ 4.66472526,  4.68940699]]), arr2(&[[-9.6819727 , -10.03086451]])],
-    //    biases: vec![arr2(&[[2.59613863], [-7.27095101]]), arr2(&[[4.87653359]])]
-    //};
-    //for (i, w) in n.weights.iter().enumerate() {
-    //    println!("{}", i);
-    //    print_matrix(w);
-    //}
-    //for (i, b) in n.biases.iter().enumerate() {
-    //    println!("{}", i);
-    //    print_matrix(b);
-    //}
-    //print_matrix(&n.feedforward(arr2(&[[1.0, 2.0, 5.0], [3.0, 4.0, 6.0]])));
+
+    let start = std::time::Instant::now();
 
     n.stochastic_gradient_descent(vec![(arr2(&[[1.0], [0.0]]), arr2(&[[1.0]])),
                                        (arr2(&[[0.0], [1.0]]), arr2(&[[1.0]])),
                                        (arr2(&[[1.0], [1.0]]), arr2(&[[0.0]])),
                                        (arr2(&[[0.0], [0.0]]), arr2(&[[0.0]]))], 1000000, 4, 0.1);
 
+    let elapsed = start.elapsed();
+    println!("Elapsed: {:?}", elapsed);
+    println!("{:.2?} epochs/s", 1000000f32 / elapsed.as_secs_f32());
+
+    println!("\nWeights: ");
     for (i, w) in n.weights.iter().enumerate() {
         println!("{}", i);
         print_matrix(w);
     }
+
+    println!("\nBiases: ");
     for (i, b) in n.biases.iter().enumerate() {
         println!("{}", i);
         print_matrix(b);
     }
 
+    println!("\nResults: ");
     println!("[1.0], [0.0]");
     print_matrix(&n.feedforward(arr2(&[[1.0], [0.0]])));
     println!("[0.0], [1.0]");
