402500742cf35884a25b534c1f659f3ae8e9de55
CuzImClicks <58398364+CuzImClicks@users.noreply.github.com>
2024-11-20 20:25:19 +0100

    inline all of them!

--- src/activation.rs
+++ src/activation.rs
@@ -9,6 +9,7 @@ pub fn sigmoid_prime(x: f64) -> f64 {
     sigmoid(x) * (1.0 - sigmoid(x))
 }
 
+#[inline(always)]
 pub fn relu(x: f64) -> f64 {
     if x > 0.0 {
         x
@@ -17,6 +18,7 @@ pub fn relu(x: f64) -> f64 {
     }
 }
 
+#[inline(always)]
 pub fn relu_prime(x: f64) -> f64 {
     if x > 0.0 {
         1.0
@@ -25,18 +27,32 @@ pub fn relu_prime(x: f64) -> f64 {
     }
 }
 
+#[inline(always)]
 pub fn leaky_relu(x: f64) -> f64 {
     if x > 0.0 { x } else { 0.01 * x }
 }
 
+#[inline(always)]
 pub fn leaky_relu_prime(x: f64) -> f64 {
     if x > 0.0 { 1.0 } else { 0.01 }
 }
 
+#[inline(always)]
 pub fn tanh(x: f64) -> f64 {
     x.tanh()
 }
 
+#[inline(always)]
 pub fn tanh_prime(x: f64) -> f64 {
     1.0 - x.tanh().powi(2)
+}
+
+#[inline(always)]
+pub fn linear(x: f64) -> f64 {
+    x
+}
+
+#[inline(always)]
+pub fn linear_prime(_x: f64) -> f64 {
+    1.0
 }
